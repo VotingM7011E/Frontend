93f40295cff1e0b4af70c7386f608f0529a66e34
Anton Nyström <antonn@ludd.ltu.se>
2026-01-04 20:30:10 +0100

    Do not minify js

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false,
+  },
   server: {
     port: 3000,
     open: true,
